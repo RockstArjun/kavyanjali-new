42f7480506fabd825c054cb5f1ee2f3a71a21373
RockstArjun <chauhanarjun064@gmail.com>
2023-02-04 23:19:25 +0530

    started

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Home/Navbar';
 
 function App() {
   return (
-    <></>
+    <>
+    <Navbar />
+    <Home />
+    </>
   );
 }
 
