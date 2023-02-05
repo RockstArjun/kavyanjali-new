449e48b8eb8f93cffc0a5e40b7b4d0048045a6de
RockstArjun <chauhanarjun064@gmail.com>
2023-02-05 23:57:49 +0530

    chalra hai bro kaam

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import "../../Assets/styles/Home.css"
+import "../../Assets/styles/Home.css";
+import kvjCalligraphy from "../../Assets/images/kvjHindi.png";
 
 export default function Home() {
   return (
     <>
-    <section>
-      
+    <section className='home'>
+      <div className='labels'>
+          <img src={kvjCalligraphy} alt='' />
+      </div>
     </section>
     </>
   )
