7cf980e59a247e252ca6091f8c0eac75f96f6849
RockstArjun <chauhanarjun064@gmail.com>
2023-02-11 01:12:06 +0530

    kaam chalra hai bro

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -5,7 +5,11 @@ export default function Home() {
   return (
     <>
     <section className='home'>
-      <h1>HINDI HAI HUM</h1>
+      <div className='home-content'>
+
+      <h1 data-aos="fade-up">Kavyanjali - The Literary Club <br /> of Graphic Era University</h1>
+      <a data-aos="fade-up" className="btn" href='/'>Register<span></span></a>
+      </div>
       <div className='labels'>
       </div>
     </section>
