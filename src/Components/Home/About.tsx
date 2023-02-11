51a25f1910b7f374813d32799ecb09b7e1b5d9db
RockstArjun <chauhanarjun064@gmail.com>
2023-02-12 01:43:18 +0530

    About Section

--- src/Components/Home/About.tsx
+++ src/Components/Home/About.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
 import "../../Assets/styles/About.css";
-
+import kvjLogo from "../../Assets/images/kvjLogo.jpg";
 export default function About() {
   return (
     <>
-    <section className='about'>
-    
-    </section>
+      <section className="about">
+        <div className="whatiskvj">
+          <div>
+            <h1 data-aos="fade-up">About Kavyanjali</h1>
+            <p data-aos="fade-up">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+              alias delectus quibusdam. Ipsum at corporis repellendus corrupti
+              nostrum reprehenderit sint id, animi ratione. Labore minima porro,
+              ab rerum accusantium incidunt!
+            </p>
+          </div>
+          <div>
+            <img data-aos="fade-up" src={kvjLogo} alt="" />
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
