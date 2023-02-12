db05f3a5b384c6bad973bbe2558d05ebf75d69d5
RockstArjun <chauhanarjun064@gmail.com>
2023-02-12 23:30:43 +0530

    kaam chalra hai

--- src/Components/Home/About.tsx
+++ src/Components/Home/About.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../Assets/styles/About.css";
 import kvjLogo from "../../Assets/images/kvjLogo.jpg";
+import joinUs from "../../Assets/images/joinUs.jpg";
 export default function About() {
   return (
     <>
@@ -17,6 +18,20 @@ export default function About() {
           </div>
           <div>
             <img data-aos="fade-up" src={kvjLogo} alt="" />
+          </div>
+        </div>
+        <div className="whyjoinus">
+          <div>
+            <h1 data-aos="fade-up">Why join us?</h1>
+            <p data-aos="fade-up">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+              alias delectus quibusdam. Ipsum at corporis repellendus corrupti
+              nostrum reprehenderit sint id, animi ratione. Labore minima porro,
+              ab rerum accusantium incidunt!
+            </p>
+          </div>
+          <div>
+            <img data-aos="fade-up" src={joinUs} alt="" />
           </div>
         </div>
       </section>
