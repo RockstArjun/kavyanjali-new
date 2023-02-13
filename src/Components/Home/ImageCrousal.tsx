52a54061c95e46ea0a7315bc047e744f67916fa8
RockstArjun <chauhanarjun064@gmail.com>
2023-02-14 00:56:29 +0530

    Kaam chalra hai

---
+++ src/Components/Home/ImageCrousal.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../../Assets/styles/ImageCrousal.css";
+
+export default function ImageCrousal() {
+  return (
+    <>
+    <section className='imageCrousal'></section>
+    </>
+  )
+}
